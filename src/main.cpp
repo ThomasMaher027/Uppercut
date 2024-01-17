6c99b24bca6f8671f988257e3a0581b9bedbc83a
ThomasMaher027 <maht1701@usherbrooke.ca>
2024-01-17 15:29:05 -0500

    Premier test
    Changer légèrment le fichier de base de Dynamixel

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,94 @@
+/*******************************************************************************
+* Copyright 2016 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+
 #include <Arduino.h>
+#include <Dynamixel2Arduino.h>
+
+//OpenRB does not require the DIR control pin.
+#define DXL_SERIAL Serial1
+#define DEBUG_SERIAL Serial
+const int DXL_DIR_PIN = -1;
+const uint8_t DXL_ID = 1;
+const float DXL_PROTOCOL_VERSION = 2.0;
+Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+
+//This namespace is required to use Control table item names
+using namespace ControlTableItem;
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  // Use UART port of DYNAMIXEL Shield to debug.
+  DEBUG_SERIAL.begin(115200);
+  while(!DEBUG_SERIAL);
+
+  // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
+  dxl.begin(57600);
+  // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+  // Get DYNAMIXEL information
+  dxl.ping(DXL_ID);
+
+  // Turn off torque when configuring items in EEPROM area
+  dxl.torqueOff(DXL_ID);
+  dxl.setOperatingMode(DXL_ID, OP_POSITION);
+  dxl.torqueOn(DXL_ID);
+
+  // Limit the maximum velocity in Position Control Mode. Use 0 for Max speed
+  dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 30);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  // Please refer to e-Manual(http://emanual.robotis.com/docs/en/parts/interface/dynamixel_shield/) for available range of value. 
+  // Set Goal Position in RAW value
+  /*dxl.setGoalPosition(DXL_ID, 1000, UNIT_DEGREE);
+
+  
+
+  while (abs(1000 - i_present_position) > 10)
+  {
+    i_present_position = dxl.getPresentPosition(DXL_ID);
+    DEBUG_SERIAL.print("Present_Position(raw) : ");
+    DEBUG_SERIAL.println(i_present_position);
+  }
+  delay(1000);*/
+  int i_present_position = 0;
+  float f_present_position = 0.0;
+  // Set Goal Position in DEGREE value
+  dxl.setGoalPosition(DXL_ID, 60, UNIT_DEGREE);
+  
+  while (abs(60 - f_present_position) > 2.0)
+  {
+    f_present_position = dxl.getPresentPosition(DXL_ID, UNIT_DEGREE);
+    DEBUG_SERIAL.print("Present_Position(degree) : ");
+    DEBUG_SERIAL.println(f_present_position);
+  }
+  delay(1000);
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
 }
+
+
+
