6835fa545121c17fdd286dc3a44e0cd3d2e99851
ThomasMaher027 <maht1701@usherbrooke.ca>
2024-01-30 17:23:45 -0500

    Mouvement en plusieurs parties

--- src/main.cpp
+++ src/main.cpp
@@ -22,14 +22,12 @@
 #define DXL_SERIAL Serial1
 #define DEBUG_SERIAL Serial
 const int DXL_DIR_PIN = -1;
-const int nb_motor = 3;
+const int nb_motor = 4;
 const int nb_movement = 3;
-const uint8_t DXL_ID[nb_motor] = {1, 3, 4}; //Motor ID
-//float home_value[nb_motor] = {0,0}; 
+const uint8_t DXL_ID[nb_motor] = {1, 3, 4, 20}; //Motor ID
 
 const float DXL_PROTOCOL_VERSION = 2.0;
 Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
-String command;
 
 //This namespace is required to use Control table item names
 using namespace ControlTableItem;
@@ -38,24 +36,30 @@ using namespace ControlTableItem;
 void setSpeed(uint8_t id, float speedPct);
 void homing();
 void printPosition();
-int choiceMovement(String command, struct _stancePosition *pos_struct);
+int choiceMovement(String command);
 void check_pos_achieved(float *target);
-void setGoal(float *pos_motor);
+void setGoal(float *pos_motor, int pos);
 
 struct _stancePosition{ // motor position (in degree)
   char stance_name[20];
-  float pos_motor[nb_motor];
+  int nb_mov;
+  float pos_motor[16];
 };
 
 struct _stancePosition stancePosition[nb_movement] = {
-  {"home",{0.0, 0.0, 0.0}},
-  {"curl",{0.0, 0.0, 90.0}},
-  {"jab",{90.0, 75.0, 25.0}}
+  {"home", 1 ,{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}},
+  {"curl", 1, {0.0, 0.0, 90.0, 0.0, 0.0, 0.0, 90.0, 0.0, 0.0, 0.0, 90.0, 0.0, 0.0, 0.0, 90.0, 0.0}},
+  {"jab", 2, {10.0, 10.0, 10.0, 10.0, 50.0, 50.0, 50.0, 50.0, 50.0, 30.0, 75.0, 25.0, 90.0, 75.0, 25.0}}
 };
 
-struct _stancePosition follow_up[1] = {
-  {"jab", {0.0, 90.0, 0.0}}
+struct movement {
+  char pos_name[20];
+  int nb_mov;
+  float pos_motor[16];
 };
+
+//struct movement curl = {"curl", 1, {0.0, 0.0, 90.0, 0.0, 0.0, 0.0, 90.0, 0.0, 0.0, 0.0, 90.0, 0.0, 0.0, 0.0, 90.0, 0.0}};
+
 
 void setup() {
   // Use UART port of DYNAMIXEL Shield to debug.
@@ -82,25 +86,31 @@ void setup() {
 
 void loop() {
   String command = Serial.readStringUntil('\n');
-  int chosen_command = choiceMovement(command, stancePosition);
-  setGoal(stancePosition[chosen_command].pos_motor);
+  int chosen_command = choiceMovement(command);
+  for (int i=0; i<stancePosition[chosen_command].nb_mov*nb_motor; i+=nb_motor){
+    setGoal(stancePosition[chosen_command].pos_motor,i);
+    check_pos_achieved(stancePosition[chosen_command].pos_motor);
+  }
+  
   check_pos_achieved(stancePosition[chosen_command].pos_motor);
   }
 
 
 
-int choiceMovement(String command, struct _stancePosition *pos_struct){
+int choiceMovement(String command){
   for (int i=0;i<nb_movement;i++){
-    if (command == pos_struct[i].stance_name){
+    if (command == stancePosition[i].stance_name){
       return i;
     } 
   }
   return 0;
 }
 
-void setGoal(float *pos_motor){
-  for(int i=0;i<nb_motor;i++){
-    dxl.setGoalPosition(DXL_ID[i], pos_motor[i], UNIT_DEGREE);
+void setGoal(float *pos_motor, int pos){
+  int mot=0;
+  for(int i=pos;i<pos+nb_motor;i++){
+    dxl.setGoalPosition(DXL_ID[mot], pos_motor[i], UNIT_DEGREE);
+    mot++;
   }
 }
 
@@ -125,7 +135,7 @@ void check_pos_achieved(float *target){
 }
 
 void homing(){
-  setGoal(stancePosition[0].pos_motor);
+  setGoal(stancePosition[0].pos_motor,0);
   check_pos_achieved(stancePosition[0].pos_motor);
 }
 
